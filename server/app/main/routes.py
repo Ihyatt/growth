ce8c671780d4a666dfb5ad9fd5c81f686292dc94
Inas Hyatt <inas.raheema@gmail.com>
2025-06-01 22:20:13 -0700

    added b

--- server/app/main/routes.py
+++ server/app/main/routes.py
@@ -1,15 +1,16 @@
-from app.app import app
+from app.main import main_bp
 from flask import jsonify, abort
 
-@app.route('/api/luna')
+from app.main import main_bp
+
+@bp.route('/api/luna')
 def luna():
     try:
         return jsonify(message="Hello, I'm Luna")
     except Exception as e:
         abort(500, description=str(e))
 
-
-@app.route('/api/romeo')
+@bp.route('/api/romeo')
 def romeo():
     try:
         return jsonify(message="Hello, I'm Romeo")
