f6b12944ecc977d336380719095c57c99d689e57
Inas Hyatt <inas.raheema@gmail.com>
2025-06-02 04:37:18 -0700

    password validaton done

--- server/app/main/routes.py
+++ server/app/main/routes.py
@@ -10,17 +10,21 @@ from app.main import bp
 def login():
     try:
         data = request.get_json()
-
+        name = data.get('name')
         email = data.get('email')
         password = data.get('password')
-
         user = User.query.filter_by(email=email).first()
+
         if not user:
             return jsonify(error="User not found"), 404
+
+        if user and not user.check_password(password):
+            return jsonify(error="Invalid password"), 404
+
         access_token = create_access_token(identity=user.id)
-        return jsonify(access_token=access_token)
+        return jsonify(message=f"welcome back, {name}", access_token=access_token)
     except Exception as e:
-        print("Error saving user:", e)
+        print("Invalid login", e)
         return jsonify(error=str(e)), 500
 
 
@@ -47,3 +51,4 @@ def register():
         db.session.rollback()
         print("Error saving user:", e)
         return jsonify(error=str(e)), 500
+    
