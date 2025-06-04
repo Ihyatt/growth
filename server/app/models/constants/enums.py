c3e034bf569009d9b7454ae3ad81e33740c98024
Inas Hyatt <inas.raheema@gmail.com>
2025-06-04 05:17:01 -0700

    further changes to models

--- server/app/models/constants/enums.py
+++ server/app/models/constants/enums.py
@@ -15,15 +15,3 @@ class AuditActionType(enum.Enum):
     APPROVED = "APPROVED"
     SET_TO_PENDING = "SET_TO_PENDING"
     REJECTED = "REJECTED"
-
-class Responses(enum.Enum):
-    NOT_AT_ALL = "NOT_AT_ALL"
-    SEVERAL_DAYS = "SEVERAL_DAYS" 
-    MORE_THAN_HALF_THE_DAYS = "MORE_THAN_HALF_THE_DAYS" 
-    NEARLY_EVERY_DAY = "NEARLY_EVERY_DAY"
-
-class ResponsesWeights(enum.Enum):
-    ONE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
--- server/app/models/practitioner_form.py
+++ server/app/models/practitioner_form.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.database import db
 from sqlalchemy import Enum 
-from app.models.constants.enums import PermissionLevel, ValidationLevel
+from app.models.constants.enums import Responses
 
 
 class PractitionerForm(db.Model):
@@ -11,7 +11,7 @@ class PractitionerForm(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     questions = db.Column(db.Text)
+    responses = db.Column(db.Text)
     practitioner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # FK to User (practitioner)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    responses = db.Column(Enum(PermissionLevel), nullable=False, default=PermissionLevel.PATIENT)
     practitioner = relationship("User", backref="practitioner_forms", foreign_keys=[practitioner_id])
