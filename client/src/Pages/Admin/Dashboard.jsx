e6778b954072cef9d775e20fa37ad22b0bce8147
Inas Hyatt <inas.raheema@gmail.com>
2025-06-04 20:31:31 -0700

    routes seem to work now to fix more routes

--- client/src/Pages/Admin/Dashboard.jsx
+++ client/src/Pages/Admin/Dashboard.jsx
@@ -1,5 +1,20 @@
+import { Link, Outlet } from 'react-router-dom';
+
+
 export function Admin() {
-    return <h2>User Details Page</h2>;
+    return (
+      <div>
+        <h1>Practitioner Dashboard</h1>
+        <nav>
+          <Link to="users">View Users</Link>
+          <Link to="users/2">View User 2</Link>
+
+        </nav>
+        <Outlet />
+      </div>
+    );
   }
 
 export default Admin;
+
+
