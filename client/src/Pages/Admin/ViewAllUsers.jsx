40190fa2daa3f5027f0e9cc53a56afd8ba87a2c4
Inas Hyatt <inas.raheema@gmail.com>
2025-06-04 19:54:20 -0700

    re added admin code

---
+++ client/src/Pages/Admin/ViewAllUsers.jsx
@@ -0,0 +1,161 @@
+import { useState } from 'react';
+import { fetchUsers, approveUser, rejectUser } from '../../../services/admin';
+import useAuthStore from '../../../stores/auth';
+
+import { USER_STATUS, USER_ROLES, USER_ACTIVE } from '../../../utils/constants';
+
+
+const Admin = () => {
+  const { jwtToken } = useAuthStore();
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState({
+    status: USER_STATUS.PENDING,
+    role: USER_ROLES.PRACTITIONER,
+    active: USER_ACTIVE.ACTIVE,
+    email: '',
+    limit: 10
+  });
+  const [pagination, setPagination] = useState({
+    page: 1,
+    limit: 10,
+    hasMore: true
+  });
+
+  const handleApproval = async (userId) => {
+    try {
+      const response = await approveUser(userId, jwtToken);
+      const data = await response.json();
+    } catch (err) {
+      setError(err.response?.data?.message || 'Failed to approve');
+    }
+  }
+
+  const handleRejection = async (userId) => {
+    try {
+      const response = await rejectUser(userId, jwtToken);
+      const data = await response.json();
+    } catch (err) {
+      setError(err.response?.data?.message || 'Failed to reject');
+    }
+  }
+
+
+  const getUsers = async (reset = false) => {
+    setLoading(true);
+    try {
+      const params = {
+        ...query,
+        page: reset ? 1 : pagination.page,
+        limit: pagination.limit,
+      };
+      console.log(query)
+      const response = await fetchUsers(params,jwtToken);
+      const data = await response.json(); 
+      const newUsers = data.users;
+      console.log(users)
+      
+      setUsers(prev => reset ? newUsers : [...prev, ...newUsers]);
+      setPagination(prev => ({
+        ...prev,
+        page: reset ? 2 : prev.page + 1,
+        hasMore: data.hasMore
+      }));
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getUsers(true); // Reset pagination on new query
+  };
+
+  const handleLoadMore = () => {
+    getUsers();
+  };
+  console.log(users)
+  return (
+    <div className="user-query">
+      <form onSubmit={handleSubmit}>
+
+     
+        <select 
+        name="status" 
+        value={query.status} 
+        onChange={(e) => setQuery({...query, status: e.target.value})}
+        >
+          {Object.values(USER_STATUS).map(status => (
+            <option key={status} value={status}>
+              {status}
+            </option>
+          ))}
+        </select>
+
+        <select 
+        name="active" 
+        value={query.active} 
+        onChange={(e) => setQuery({...query, active: e.target.value})}
+        >
+        {Object.values(USER_ACTIVE).map(active => (
+          <option key={active} value={active}>
+            {active}
+          </option>
+        ))}
+      </select>        
+        <select 
+        name="role" 
+        value={query.role} 
+        onChange={(e) => setQuery({...query, role: e.target.value})}
+        >
+          {Object.values(USER_ROLES).map(role => (
+            <option key={role} value={role}>
+              {role}
+            </option>
+          ))}
+        </select>
+
+        <input
+          type="text"
+          placeholder="Search by email"
+          value={query.search}
+          onChange={(e) => setQuery({...query, search: e.target.value})}
+        />
+
+        <button type="submit" disabled={loading}>
+          {loading ? 'Searching...' : 'Search'}
+        </button>
+      </form>
+
+      <div className="user-list">
+        {users.map(user => (
+          
+          <div key={user.id} className="user-card">
+          <button key={user.id + 1} onClick={() => handleApproval(user.id)}>approve</button>
+          <button key={user.id + 2} onClick={() => handleRejection(user.id)}>reject</button>
+            <p>email: {user.email}</p>
+            <p>Role: {user.permission}</p>
+            <p>Validation Status: {user.is_validated}</p>
+            <p>Activity Status: {user.is_active}</p>
+          </div>
+        ))}
+      </div>
+
+      {pagination.hasMore && (
+        <button 
+          onClick={handleLoadMore} 
+          disabled={loading}
+          className="load-more"
+        >
+          {loading ? 'Loading...' : 'Load More'}
+        </button>
+      )}
+      {error && <p className="login-error">{error}</p>}
+    </div>
+  );
+};
+
+export default Admin;
